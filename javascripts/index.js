d04b313cc902144dca85d97c325de1ce7a7478cf
Collus Watson <cklwatsonjr@gmail.com>
2022-02-07 21:35:14 -0500

    add displayBlog to display newly created blogs

--- javascripts/index.js
+++ javascripts/index.js
@@ -7,6 +7,14 @@ const blogCreateForm = () => document.getElementById('createBlogForm'); //grab b
 const blogTitleInput = () => document.getElementById('blogTitleInput');
 const blogAuthorInput = () => document.getElementById('blogAuthorInput');
 const blogContentInput = () => document.getElementById('blogContentInput');
+const blogCards = () => document.getElementById('blogCards')
+function validBlog(event){
+    if(!blogTitleInput().value || !blogAuthorInput().value || !blogContentInput().value){
+        console.log('error')
+    }else{
+        createBlog(event)
+    }
+}
 
 function createBlog(event){
     let blogTitle = blogTitleInput().value;
@@ -19,11 +27,24 @@ function createBlog(event){
         content: blogContent,
         // created: `${Date().getMonth() + Date().getMonth()}`
     }
-    storeBlog(newBlog)
+    allBlogs.push(newBlog); //store newBlog
     displayBlog(newBlog)
 }
 
+function displayBlog(blog){
+    let div = document.createElement('div')
+    div.classList.add('blogCard')
+    div.innerHTML = `
+    <h2 id="blogTitle">${blog.title}</h2>
+    <h3 id="blogAuthor">${blog.author}</h3>
+    <p id="blogContent">${blog.content}</p>
+    <br>
+    <button class="editBtn" type="submit" name="action" id="editBlog">Edit</button>
+    <button class="dltBtn" type="submit" name="action" id="deleteBlog">Delete</button>`
+    blogCards().appendChild(div);
+}
+
 blogCreateForm().addEventListener('submit', (event) => {
-    event.preventDefault();
-    createBlog(event);
+    event.preventDefault(); //stop auto refrech
+    validBlog(event); //will check if vlog inputs have information
 })
