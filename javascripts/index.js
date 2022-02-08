e7cf7ccc35932f202f2f9bbbadeeeda0715e3959
Collus Watson <cklwatsonjr@gmail.com>
2022-02-07 21:13:37 -0500

    add js to get blog form information

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,1 +1,29 @@
 // Page for JS content
+
+const allBlogs = [];
+let id = 0;
+
+const blogCreateForm = () => document.getElementById('createBlogForm'); //grab blog form
+const blogTitleInput = () => document.getElementById('blogTitleInput');
+const blogAuthorInput = () => document.getElementById('blogAuthorInput');
+const blogContentInput = () => document.getElementById('blogContentInput');
+
+function createBlog(event){
+    let blogTitle = blogTitleInput().value;
+    let blogAuthor = blogAuthorInput().value;
+    let blogContent = blogContentInput().value;
+    // let time = () => {new Date().now}
+    const newBlog = {
+        title: blogTitle,
+        author: blogAuthor,
+        content: blogContent,
+        // created: `${Date().getMonth() + Date().getMonth()}`
+    }
+    storeBlog(newBlog)
+    displayBlog(newBlog)
+}
+
+blogCreateForm().addEventListener('submit', (event) => {
+    event.preventDefault();
+    createBlog(event);
+})
