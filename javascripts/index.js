27130f19f1d7c1ece92711e2148bd7e78941a256
Collus Watson <cklwatsonjr@gmail.com>
2022-02-12 00:15:53 -0500

    finish tranferring code/ restructuring code

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,33 +2,65 @@ const createCardForm = () => document.getElementById('createBlogForm');
 const titleInput = () => document.getElementById('blogTitleInput');
 const authorInput = () => document.getElementById('blogAuthorInput');
 const contentInput = () => document.getElementById('blogContentInput');
-const cardSelector = () => document.getElementById('selectBlogs');
+
 const cardContainer = () => document.getElementById('blogCards');
+const cardSelector = () => document.getElementById('selectBlogs');
+let currentCards = cardSelector().value;
 
-// Grabs created buttons in created cards
-const editBtn = () => document.getElementById('editBtn') //edit button
-const saveBtn = () => document.getElementById('saveBtn') //save button
-const cancelBtn = () => document.getElementById('cancelBtn') //cancel button
-const removeBtn = () => document.getElementById('removeBtn') //remove button
-const deleteBtn = () => document.getElementById('deleteBtn') //delete button
-
-
-// FETCH REQUESTS
+// FETCH REQUESTS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 //fetch request data.. gets blog data from server
-function fetchCardData(selectBlog = 'currentBlogs'){ //gets data from server and creates card
-    fetch(`http://localhost:3000/${selectBlog}`)
+function fetchCardData(selectContainer = currentCards){ //gets data from server and creates card
+    cardContainer().innerHTML = ''; //empties container of all cards
+    fetch(`http://localhost:3000/${selectContainer}`)
     .then(res => res.json())
     .then(data => {
         let array = data;
         array.forEach(object => {
-            createCard(object)    
+            createCard(object, selectContainer)    
         });
     })
     .catch(err => console.log('Err, failed to fetchCardData', err))
 }
+function addData(object, selectContainer){
+    fetch(`http://localhost:3000/${selectContainer}`,{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(object)
+    })
+    .then(res => res.json())
+    .then(data => console.log('Success add:', data))
+    .catch(err => console.log('Fail add:', err))
+}
+function updateData(object, selectContainer){
+    console.log(object, selectContainer)
+    fetch(`http://localhost:3000/${selectContainer}/${object.id}`,{
+        method:'PATCH',
+        headers:{
+            'Content-type':'application/json'
+        },
+        body: JSON.stringify(object)
+    })
+    .then(res => res.json())
+    .then(data => console.log('Success update:', data))
+    .catch(err => console.log('Failure: ', err))
+}
+function deleteData(object, selectContainer = currentCards){
+    fetch(`http://localhost:3000/${selectContainer}/${object.id}`,{
+        method: 'DELETE',
+        headers:{
+            'Content-Type':'application/json'
+        }
+    })
+    .then(res => res.json())
+    .then(data => console.log('Success delete', data))
+    .catch(err => console.log('Fail delete', err))
+}
 
-// FUNCTIONS
-function createCard(object){ //create card and fills in object data, adds to html container
+// FUNCTIONS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+function createCard(object, container){ //create card and fills in object data, adds to html container
+
     let card = document.createElement('div')
     card.id = object.id;
     card.innerHTML = `
@@ -40,18 +72,94 @@ function createCard(object){ //create card and fills in object data, adds to htm
     <button id="saveBtn" type="submit">Save</button>
     <button id="cancelBtn" type="submit">Cancel</button>
     <button id="removeBtn" type="submit">Remove</button>
+    <button id="restoreBtn" type="submit">Restore</button>
     <button id="deleteBtn" type="submit">Delete</button>
     `;
+    card.querySelector('#saveBtn').style.display = 'none'
+    card.querySelector('#cancelBtn').style.display = 'none'
+    card.querySelector('#restoreBtn').style.display = 'none'
+
+    if(container == 'removedCards'){
+        card.querySelector('#removeBtn').style.display = 'none'
+        card.querySelector('#restoreBtn').style.display = ''
+    }
+
+    // created event listeners for buttons
+    card.querySelector('#editBtn').addEventListener('click', (e)=>{
+        e.preventDefault();
+        editCardData(card, object);
+    })    
+    card.querySelector('#removeBtn').addEventListener('click', (e)=>{ //delete from current, add to removed container
+        e.preventDefault();
+        addData(object, 'removedCards')
+        deleteData(object, 'currentCards')
+    })
+    card.querySelector('#restoreBtn').addEventListener('click', (e)=>{ //add object to current and deletes from removed
+        e.preventDefault();
+        addData(object, 'currentCards')
+        deleteData(object, 'removedCards')
+    })
+    card.querySelector('#deleteBtn').addEventListener('click', (e)=>{ //deletes from current container after confirming
+        e.preventDefault();
+        deleteCardData(object);
+    })    
+
     cardContainer().appendChild(card)
 }
 
+function editCardData(card, object){
+    card.querySelector('#editBtn').style.display = 'none'
+    card.querySelector('#removeBtn').style.display = 'none'
+    card.querySelector('#deleteBtn').style.display = 'none'
+    card.querySelector('#restoreBtn').style.display = 'none'
+    card.querySelector('#saveBtn').style.display = ''
+    card.querySelector('#cancelBtn').style.display = ''
+    card.querySelector('#cardTitle').contentEditable = 'true';
+    card.querySelector('#cardAuthor').contentEditable = 'true';
+    card.querySelector('#cardContent').contentEditable = 'true';
+
+    card.querySelector('#saveBtn').addEventListener('click', (e)=>{
+        e.preventDefault();
+        let newTitle = card.querySelector('#cardTitle');
+        let newAuthor = card.querySelector('#cardAuthor');
+        let newContent = card.querySelector('#cardContent');
+        object.title = newTitle.innerHTML
+        object.author = newAuthor.innerHTML
+        object.content = newContent.innerHTML
+        updateData(object, currentCards);
+        fetchCardData(currentCards)
+    })
+    card.querySelector('#cancelBtn').addEventListener('click', (e)=>{
+        e.preventDefault();
+        fetchCardData(currentCards)
+    })
+}
+
+
+
+//EVENT LISTENERS
+createCardForm().addEventListener('submit', (e)=>{ //create new card, adds card to current card data
+    e.preventDefault();
+    console.log('create card')
+    let newCard = {
+        title: titleInput().value,
+        author: authorInput().value,
+        content: contentInput().value
+    }
+    addData(newCard, 'currentCards');
+})
+cardSelector().addEventListener('change', (e)=>{
+    e.preventDefault();
+    cardSelector().value = e.target.value
+    currentCards = cardSelector().value
+    console.log(currentCards)
+    fetchCardData(currentCards)
+})
+
 document.addEventListener('DOMContentLoaded', (event) => {
-    fetchCardData();
+    fetchCardData(); //fetch request with current value of selector
     console.log('DOM fully loaded and parsed');
 });
-
-
-
 
 
 
